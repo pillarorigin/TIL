c169ae328791682dae30eb29cb0a0d4d7f74073b
pillarorigin <pillar_origin@naver.com>
2019-11-07 15:15:40 +0900

    modified gif_tv.js

--- day02/giphy/gif_tv.js
+++ day02/giphy/gif_tv.js
@@ -8,7 +8,6 @@
 // includeJS('./key.js');
 
 
-
 // 여러가지 키워드로 검색된 결과를 1번 저장하고 
 const contents = ['car', 'cat', 'wow', 'coin'];
 const keyword = contents[Math.floor(Math.random()*contents.length)]
@@ -22,21 +21,25 @@ function searchTV(keyword) {
     GiphyAJAXCall.addEventListener('load', e => {
         const rawData = e.target.response;
         const parsedData = JSON.parse(rawData); //json으로 parsing
-        console.log(parsedData);
-    })
+        pushToTvDom(parsedData);
+    });
 };
 
 // 저장된 값에서 하나씩 꺼내서 보여주기 (랜덤하게)
-const tvArea = document.querySelector('#js-tv-area');
-function TvArea() {
+    const tvArea = document.querySelector("#js-tv-area")
     //contents의 키워드들을 랜덤하게, 그림 하나만, 3초 릴레이로 보여주기
-    tvArea.innerHTML = null;
-    //const dataSet = parsedData.data;
-    const dataSet = setInterval(function(){
-        const ImageURL = ImageData.images.fixed_height.url;
-        const alt = ImageData.title;
-        tvArea.innerHTML = `<img src="${ImageURL}" alt="${alt}" />`
-    }, 3000)
-};
+    function pushToTvDom(parsedData) {
+      tvArea.innerHTML = null;
+      const dataSet = parsedData.data;
+      let i = 0;
+    
+      setInterval(function() {
+        const imageURL = dataSet[i].images.fixed_height.url;
+        tvArea.innerHTML = `<img src="${imageURL}" class="img-center"/>`;
+        i++;
+        if (i >= dataSet.length) i = 0;
+      }, 3000)
+    }
+    
 
 document.addEventListener('DOMContentLoaded', searchTV)
