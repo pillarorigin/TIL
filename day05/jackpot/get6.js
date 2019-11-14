ee6da76570f1c0df29709c9f50eab71cfac07f12
pillarorigin <pillar_origin@naver.com>
2019-11-14 15:22:25 +0900

    add cheack6.js

--- day05/jackpot/get6.js
+++ day05/jackpot/get6.js
@@ -5,10 +5,26 @@ function getLottoData(drwNo) {
     const url =`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}` ;
     return new Promise((resolve, reject)=> {
         request.get(url, (err, response, body) => {
-            console.log(body)
+            // console.log(body)
+            const lottoData = JSON.parse(body);
+            // console.log(lottoData);
+            const realNumber = [];
+            let bnusNo = 0;
+            //js for of문법
+            for(const [key, value] of Object.entries(lottoData)){
+                console.log('현재 key-value');
+                console.log(key);
+                console.log(value);
+                console.log('==================')
+                if (key.includes('drwt'))realNumber.push(value);
+                else if (key === 'bnusNo') bnusNo = value;
+                realNumber.sort((a,b)=> a-b);
+            };
+            // resolve({bnusNo:bnusNo, realNumber:realNumber}); //ES5
+            resolve({bnusNo, realNumber});
         })
     })
 }
 
-module.exports = getLottoData(884);  //getLottoData()함수를 밖에서 사용하겠다.
+module.exports = getLottoData;  //getLottoData()함수를 밖에서 사용하겠다.
 // module.exports = getLottoData();   -> getLottoData()함수를 실행한 결과를 리턴한다는 의미
