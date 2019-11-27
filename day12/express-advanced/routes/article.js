3194945aa27f80bd96e624aa59486f371c639d77
pillarorigin <pillar_origin@naver.com>
2019-11-27 13:50:43 +0900

    [ADD] express advanced initialzation

---
+++ day12/express-advanced/routes/article.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+
+const articles = [
+    {id:1, author:1, content: '아이디어톤 기획안 제출하기'},
+    {id:2, author:3, content: '5시 까지'},
+];
+
+router.get('/:id', (req, res) => {
+    res.send(articles.find(article => article.id === parseInt(req.params.id)));
+});
+
+router.post('/', (req, res) => {
+    const newArticle = {
+        id:3, author:6, content: '월요일 발표한다는데요'
+    }
+    articles.push(newArticle);
+    res.send(articles)
+});
+
+//router export
+module.router = router;
